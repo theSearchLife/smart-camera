42c1d26d6e2e248a47cf9dc5d4ea246288354b3c
OMEGAMAX10 <bogdan.marghescu9@gmail.com>
2024-03-08 01:26:14 +0200

    small change to main script for running the app

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Start each command in the background
 python3 Classify/download_model.py Config.json &
+sleep 20
 python3 Capture/picamera.py Config.json &
 python3 Motion/motion.py Config.json &
 python3 Classify/classify_image.py Config.json &
 python3 Upload/upload_edge.py Config.json &
 python3 Upload/upload_telegram.py Config.json &
-
-# Wait for all background jobs to finish
 wait
