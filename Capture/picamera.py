4bd38cc0e169862182ff9207f6b26a52f977cdcf
OMEGAMAX10 <bogdan.marghescu9@gmail.com>
2024-03-10 01:08:06 +0200

    Added scheduled stream capturing, the hours can be set in Config.json

--- Capture/picamera.py
+++ Capture/picamera.py
@@ -11,6 +11,27 @@ import cv2
 #used for closing
 picam2=None
 
+def is_time_to_capture(start_time_str, end_time_str):
+    """
+    Check if the current time is within a specified range, potentially spanning over midnight.
+    Args:
+        start_time_str (str): The start time in "HH:MM" format.
+        end_time_str (str): The end time in "HH:MM" format.
+    Returns:
+        bool: True if the current time is within the range, False otherwise.
+    """
+    now = datetime.datetime.now()
+    start_time_params = [int(x) for x in start_time_str.split(":")]
+    end_time_params = [int(x) for x in end_time_str.split(":")]
+    start_time = now.replace(hour=start_time_params[0], minute=start_time_params[1], second=0, microsecond=0)
+    end_time = now.replace(hour=end_time_params[0], minute=end_time_params[1], second=59, microsecond=999999)
+    if end_time <= start_time:
+        if now < end_time:
+            start_time -= datetime.timedelta(days=1)
+        else:
+            end_time += datetime.timedelta(days=1)
+    return start_time <= now < end_time
+
 def keep_fps( start_time,final_time, fps):
     fps_real = 1 / (final_time - start_time)
     if fps_real < fps:
@@ -27,18 +48,19 @@ def handler(signum, frame):
 def main(argv):
     global picam2
     while True:
-
         try:
             opts, args = getopt.getopt(argv, "h", ["--help"])
         except getopt.GetoptError:
             sys.exit(2)
-
         config_loader.load_config(args[0])
+        start_time_str = config_loader.get_value("CAPTURE_STARTTIME")
+        end_time_str = config_loader.get_value("CAPTURE_ENDTIME")
+        if not is_time_to_capture(start_time_str, end_time_str):
+            time.sleep(60)
+            continue
         if config_loader.get_value("CAPTURE_PICAMERA_FPS") == 0:
             try:
-          
                 fps_needed = float(config_loader.get_value("CAPTURE_STREAM_FPS"))
-
                 vcap = cv2.VideoCapture(config_loader.get_value("CAPTURE_STREAM_URL"), cv2.CAP_FFMPEG)
                 while True:
                     start_time=time.time()
