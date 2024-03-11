2a2c961d5214d0973e3a37cd5718e2b4fdf74050
OMEGAMAX10 <bogdan.marghescu9@gmail.com>
2024-03-11 20:44:13 +0200

    added schedule on weekdays

--- Capture/picamera.py
+++ Capture/picamera.py
@@ -7,36 +7,61 @@ import sys, getopt
 import signal
 import config_loader
 import cv2
+import threading
 from telegram import Bot
-from telegram.ext import CommandHandler, Updater
 import asyncio
 #used for closing
 picam2=None
+is_capture_time = False
 
 async def send_telegram_message(channel_id, token, message):
     bot = Bot(token=token)
     await bot.send_message(chat_id=channel_id, text=message)
 
-def is_time_to_capture(start_time_str, end_time_str):
+def is_time_to_capture():
     """
-    Check if the current time is within a specified range, potentially spanning over midnight.
-    Args:
-        start_time_str (str): The start time in "HH:MM" format.
-        end_time_str (str): The end time in "HH:MM" format.
+    Determine if the current time is within the scheduled capture range for the current day of the week.        
     Returns:
         bool: True if the current time is within the range, False otherwise.
     """
     now = datetime.datetime.now()
-    start_time_params = [int(x) for x in start_time_str.split(":")]
-    end_time_params = [int(x) for x in end_time_str.split(":")]
-    start_time = now.replace(hour=start_time_params[0], minute=start_time_params[1], second=0, microsecond=0)
-    end_time = now.replace(hour=end_time_params[0], minute=end_time_params[1], second=59, microsecond=999999)
-    if end_time <= start_time:
-        if now < end_time:
-            start_time -= datetime.timedelta(days=1)
-        else:
-            end_time += datetime.timedelta(days=1)
-    return start_time <= now < end_time
+    day_of_week = now.strftime('%A').upper()  # Get the current day of the week in all caps, e.g., "MONDAY"
+    start_time_str = config_loader.get_value(f"CAPTURE_SCHEDULE_{day_of_week}_START")
+    end_time_str = config_loader.get_value(f"CAPTURE_SCHEDULE_{day_of_week}_END")
+    # Check if both start and end times are empty first
+    if start_time_str == "" and end_time_str == "":
+        print(f"Capture schedule for {day_of_week} is not set.")
+        return False
+    # Then, set default values for missing start or end times
+    if start_time_str == "":
+        start_time_str = "00:00"
+    if end_time_str == "":
+        end_time_str = "23:59"
+    # Parse and construct datetime objects for comparison
+    start_time = datetime.datetime.strptime(start_time_str, '%H:%M').time()
+    end_time = datetime.datetime.strptime(end_time_str, '%H:%M').time()
+    current_time = now.time()
+    # Adjust for dates when end time is next day
+    if start_time >= end_time:
+        raise ValueError("Start time cannot be later than end time for the day.")
+    # Check if the current time is within the start and end time range
+    return start_time <= current_time <= end_time
+
+def update_capture_status(update_interval=30):
+    """
+    Periodically update the global capturing status.
+    
+    Args:
+        config_loader: Configuration loader to fetch schedule.
+        update_interval (int): How often to update the status, in seconds.
+    """
+    global is_capture_time
+    while True:
+        try:
+            is_capture_time = is_time_to_capture()
+        except Exception as e:
+            print(f"Error updating capture status: {e}")
+        time.sleep(update_interval)
 
 def keep_fps( start_time,final_time, fps):
     fps_real = 1 / (final_time - start_time)
@@ -61,10 +86,10 @@ def main(argv):
         config_loader.load_config(args[0])
         TOKEN = config_loader.get_value("ALERT_TOKEN")
         channel_id = config_loader.get_value("ALERT_CHANNELID")
-        start_time_str = config_loader.get_value("CAPTURE_STARTTIME")
-        end_time_str = config_loader.get_value("CAPTURE_ENDTIME")
-        if not is_time_to_capture(start_time_str, end_time_str):
-            time.sleep(60)
+        status_thread = threading.Thread(target=update_capture_status, args=())
+        status_thread.start()
+        if not is_capture_time:
+            time.sleep(5)
             continue
         if config_loader.get_value("CAPTURE_PICAMERA_FPS") == 0:
             try:
@@ -76,12 +101,13 @@ def main(argv):
                     if ret == False:
                         print("Frame is empty, stream is not available!")
                         raise Exception("Frame is empty, stream is not available!")
-                        break
                     else:
                         now = datetime.datetime.now()
                         cv2.imwrite(config_loader.get_value("DATAFOLDER")+'/captured/'+str(now.hour)+str(now.minute)+str(now.second)+str(randint(0, 100))+'.jpg', frame)
                     keep_fps(start_time,time.time(),fps_needed)
-
+                    if not is_capture_time:
+                        vcap.release()
+                        break
                 vcap.release()
             except Exception as ex:
                 print(f'Camera stream {config_loader.get_value("CAPTURE_STREAM_URL")} is not available or failed: {ex}, retrying in 5 minutes')
@@ -114,6 +140,9 @@ def main(argv):
                         print(metadata)
 
                     keep_fps(start_time,time.time(),fps_needed)
+                    if not is_capture_time:
+                        picam2.close()
+                        break
             except Exception as ex:
                 print(f'Picamera is not available or failed: {ex}, retrying in 5 minutes')
                 loop = asyncio.get_event_loop()
