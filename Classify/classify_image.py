5e21028fae0df1d4760b9672d281cfd9cf72f11a
OMEGAMAX10 <bogdan.marghescu9@gmail.com>
2024-03-19 22:25:44 +0200

    changed the logic of uploading the pictures on telegram by only uploading those with detections in them

--- Classify/classify_image.py
+++ Classify/classify_image.py
@@ -30,12 +30,9 @@ def main(argv):
             while True:
                 dir_name=config_loader.get_value("DATAFOLDER")+'/motion/'
                 # Get list of all files only in the given directory
-                list_of_files = filter( lambda x: os.path.isfile(os.path.join(dir_name, x)),
-                            os.listdir(dir_name) )
+                list_of_files = filter(lambda x: os.path.isfile(os.path.join(dir_name, x)), os.listdir(dir_name))
                 # Sort list of files based on last modification time in ascending order
-                list_of_files = sorted( list_of_files,
-                                        key = lambda x: os.path.getmtime(os.path.join(dir_name, x))
-                                        )
+                list_of_files = sorted(list_of_files, key = lambda x: os.path.getmtime(os.path.join(dir_name, x)))
                 # Iterate over sorted list of files and print file path 
                 # along with last modification time of file 
                 for file_name in list_of_files:
@@ -80,11 +77,13 @@ def main(argv):
                                 cropped = cv2.rectangle(cropped, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
                                 if config_loader.get_value("DEBUG") == 1:
                                     cv2.imwrite(config_loader.get_value("DATAFOLDER")+'/debug/detect.jpg',cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
+                                now = datetime.datetime.now()
+                                cv2.imwrite(config_loader.get_value("DATAFOLDER")+'/detectedTelegram/'+str(now.hour)+str(now.minute)+str(now.second)+str(randint(0, 100))+'.jpg',  cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
+                                
                     now = datetime.datetime.now()
 
                     if config_loader.get_value("DETECTION_UPLOADEDGE") == 1:
                         cv2.imwrite(config_loader.get_value("DATAFOLDER")+'/detected/'+str(now.hour)+str(now.minute)+str(now.second)+str(randint(0, 100))+'.jpg',  cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
-                    cv2.imwrite(config_loader.get_value("DATAFOLDER")+'/detectedTelegram/'+str(now.hour)+str(now.minute)+str(now.second)+str(randint(0, 100))+'.jpg',  cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
                     #cv2.imwrite("detected.jpg",cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR)) 
                     os.remove(file_path)      
                 time.sleep(5) 
