64cc28928d75226c034472e06d0850f88f7a2656
OMEGAMAX10 <bogdan.marghescu9@gmail.com>
2024-05-10 21:11:36 +0300

    changed capture time to motion time

--- Motion/motion.py
+++ Motion/motion.py
@@ -112,6 +112,7 @@ def main(argv):
                                 continue
                             if RoisClass.overlap(frame1,x,y,w,h):
                                 now = datetime.datetime.now()
+                                cv2.putText(frame1, now.strftime("%H:%M:%S"), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
                                 cv2.imwrite(os.path.join(config_loader.get_value("DATAFOLDER"), 'motion', f'{now.strftime("%Y%m%d%H%M%S%f")}.jpg'),  cv2.cvtColor(frame1, 0))
                                 if config_loader.get_value("DEBUG") == 1:
                                     print(f"Motion detected in {file_name_to_del}, saved in " + os.path.join(config_loader.get_value("DATAFOLDER"), 'motion', f'{now.strftime("%Y%m%d%H%M%S%f")}.jpg'))
